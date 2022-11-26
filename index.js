1aa758f8ff782a91b584cdccbdf49265743155ee
Saidur Rahman <saidurrahman1473@gmail.com>
2022-11-26 20:49:37 +0600

    add jsonwebtoken

--- index.js
+++ index.js
@@ -23,19 +23,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
     const carsCollection = client.db('fastResale').collection('allCars')
 //                                                                             .updateMany(
-//                                                                              {currentDate:9},
+//                                                                              {currentDate:true},
 //                                                                              {$set: {
 //                                                                                  curentDate: new Date()
 //                                                                             }}
 //                                                                              )
 
+const bookingsCollection = client.db('fastResale').collection('bookings')
+const usersCollection = client.db('fastResale').collection('users')
+
  
     
 
     app.get('/allCars', async(req,res)=>{
+        // const selectedCarName = req.body.carName;
         const query = {};
         const result = await carsCollection.find(query).toArray()
-        console.log(result);
+        // const bookingQuery = {selectedCarName:carName}
+        // console.log(bookingQuery);
         res.send(result)
     })
     app.get('/allCars/:id',async(req,res)=>{
@@ -44,18 +49,64 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
        const cars = await carsCollection.findOne(query)
        res.send(cars)
     })
-    // app.put('/allCars/:id',async(req,res)=>{
-    //    const id = req.params.id;
-    //    const filter = {_id: ObjectId(id)}
-    //    const options = {upsert:true}
-    //    const updateDoc = {
-    //     $currentDate:{ post:true }
-    //    }
-    //    const cars = await carsCollection.findOne(filter, updateDoc,options)
-    //    res.send(cars)
-    // })
+
+    app.get('/bookings', async(req, res)=>{
+        const email = req.query.email;
+        const query = { email: email};
+        const bookings = await bookingsCollection.find(query).toArray()
+        res.send(bookings)
+
+    })
+    
+    //POST
+    app.post('/bookings', async(req,res)=>{
+        const booking = req.body;
+        console.log(booking);
+        const query = {
+            carName: booking.carName,
+            email:booking.email
+        }
+        const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+        if(alreadyBooked.length){
+
+            const message = `You Already have a Booked ${booking.carName}`
+            return res.send({acknowledged: false, message})
+
+        }
 
 
+        const result = await bookingsCollection.insertOne(booking)
+        res.send(result)
+    })
+    //JWT
+    app.get('/jwt', async(req, res)=>{
+        const email = req.query.email;
+        const query = {email:email};
+        const user = await usersCollection.findOne(query)
+        if(user){
+            const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '7d'})
+            return res.send({accessToken: token})
+        }
+        console.log(user);
+        res.status(403).send({accessToken: ''})
+    })
+
+
+
+
+
+
+
+
+
+    //Users
+    app.post('/users',async(req,res)=>{
+        const user = req.body;
+        console.log(user);
+        const result = await usersCollection.insertOne(user);
+        res.send(result)
+    })
 
 
 
